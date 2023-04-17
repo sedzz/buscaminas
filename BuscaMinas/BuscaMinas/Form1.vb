1b730c531e821a326369be110fd701087dcc3898
Sebas Diez <sebas10456diez@gmail.com>
2023-04-17 08:40:25 +0200

    Actualizacion 3.0 (Dificultades)

--- BuscaMinas/BuscaMinas/Form1.vb
+++ BuscaMinas/BuscaMinas/Form1.vb
@@ -1,4 +1,6 @@
-﻿Public Class Form1
+﻿Imports System.CodeDom
+
+Public Class Form1
     Public matriz(dificultad.PosX, dificultad.PosY) As Button
     Private posicionDeBombas(dificultad.Bombas - 1) As String
     Dim x As Integer = 0
@@ -6,7 +8,55 @@
     Private BombasGeneradas As Boolean = False
 
     Private Sub Form1_Show(sender As Object, e As EventArgs) Handles MyBase.Load
+        CrearTablero()
+    End Sub
+    Private Sub Boton_Click(sender As Object, e As EventArgs)
+        Dim boton As Button = TryCast(sender, Button)
+        Dim rnd As New Random
+        Dim posicionX, posicionY, posicionMenosX, posicionMenosY As Integer
 
+        If TypeOf boton Is Button And BombasGeneradas = False Then
+
+            For i = 0 To dificultad.Bombas - 1
+                posicionX = rnd.Next(dificultad.PosX)
+                posicionY = rnd.Next(dificultad.PosY)
+                If Not posicionDeBombas.Contains(posicionX & posicionY) AndAlso Not $"btn{posicionX}_{posicionY}".Equals(boton.Name) Then
+                    matriz(posicionX, posicionY).Text = "x"
+                    posicionDeBombas(i) = posicionX & posicionY
+                    matriz(posicionMenosX, posicionMenosY).Tag = "bomba"
+                Else
+                    i -= 1
+                End If
+            Next
+
+        End If
+
+        BombasGeneradas = True
+
+        If boton.Text = "x" Then
+            Dim imagePath As String = "C:\Users\sebas\Desktop\Dam1\Programacion\a\BuscaMinas/bus.png"
+            Dim Image As Image = Image.FromFile(imagePath)
+            boton.Image = Image
+
+
+
+            For Each bomba As Button In matriz ' = 2_8
+                If bomba.Tag = "bomba" Then
+                    bomba.Image = Image
+                End If
+                'matriz(posicionX, posicionY).Image = Image
+            Next
+        End If
+
+
+
+    End Sub
+
+
+
+
+
+    Function CrearTablero()
         For i = 0 To dificultad.PosX - 1 'Recorre las filas
 
             For j = 0 To dificultad.PosY - 1 'Recorre las columnas
@@ -22,41 +72,5 @@
             x += 30
             y = 0
         Next i
-
-    End Sub
-
-    Private Sub Boton_Click(sender As Object, e As EventArgs)
-        Dim boton As Button = TryCast(sender, Button)
-        Dim rnd As New Random
-        Dim posicionX, posicionY, posicionMenosX, posicionMenosY As Integer
-
-        If TypeOf boton Is Button And BombasGeneradas = False Then
-
-            For i = 0 To dificultad.Bombas - 1
-                posicionX = rnd.Next(dificultad.PosX)
-                posicionY = rnd.Next(dificultad.PosY)
-                If Not posicionDeBombas.Contains(posicionX & posicionY) AndAlso Not $"btn{posicionX}_{posicionY}".Equals(boton.Name) Then
-                    matriz(posicionX, posicionY).Text = "x"
-                    posicionDeBombas(i) = posicionX & posicionY
-                Else
-                    i -= 1
-
-                End If
-            Next
-
-        End If
-
-        BombasGeneradas = True
-
-        If boton.Text = "x" Then
-            Dim imagePath As String = "C:\Users\sebas\OneDrive\Escritorio\Dam1\Programacion\a\BuscaMinas/bus.png"
-            Dim Image As Image = Image.FromFile(imagePath)
-            boton.Image = Image
-
-            For Each posicionDeUnaBomba In posicionDeBombas ' = 2_8
-                matriz(posicionX, posicionY).Image = Image
-            Next
-        End If
-
-    End Sub
+    End Function
 End Class
