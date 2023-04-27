e5ea4e73a81d9fadeff4d080132eec9467e8c509
Sebas Diez <infernus10456ps3d@gmail.com>
2023-04-27 09:07:55 +0200

    Version 3.2 guardar usuarios(prueba)

---
+++ ClasesBM/Usuario.vb
@@ -0,0 +1,30 @@
+﻿Public Class Usuario
+    Implements IEquatable(Of Usuario)
+
+    Public Property nombre As String
+    Public Property contraseña As String
+    Public Property puntuacion As Integer
+
+    Sub New(nombre As String, contraseña As String)
+        Me.nombre = nombre
+        Me.contraseña = contraseña
+    End Sub
+
+    Public Overrides Function Equals(obj As Object) As Boolean
+        Return Equals(TryCast(obj, Usuario))
+    End Function
+
+    Public Overloads Function Equals(other As Usuario) As Boolean Implements IEquatable(Of Usuario).Equals
+        Return other IsNot Nothing AndAlso
+               nombre.ToLower = other.nombre.ToLower AndAlso
+               contraseña = other.contraseña
+    End Function
+
+    Public Shared Operator =(left As Usuario, right As Usuario) As Boolean
+        Return EqualityComparer(Of Usuario).Default.Equals(left, right)
+    End Operator
+
+    Public Shared Operator <>(left As Usuario, right As Usuario) As Boolean
+        Return Not left = right
+    End Operator
+End Class
---
+++ ClasesBM/Usuarios.vb
@@ -0,0 +1,35 @@
+﻿Imports System.IO
+
+Public Class Usuarios
+    Public Property UsuariosTotales As List(Of Usuario)
+
+    Sub New()
+        Me.UsuariosTotales = New List(Of Usuario)
+    End Sub
+
+    Public Function AñadirUsuario(nombre As String, contraseña As String) As Boolean
+        Dim ruta As String = "../../"
+
+
+
+
+        Dim usuario As New Usuario(nombre, contraseña)
+        Dim posicionUsuario = UsuariosTotales.IndexOf(usuario)
+        If posicionUsuario = -1 Then
+            UsuariosTotales.Add(usuario)
+            Return True
+        End If
+        Return False
+    End Function
+
+    Public Function ConectarUsuario(nombre As String, contraseña As String) As Boolean
+        Dim usuario As New Usuario(nombre, contraseña)
+        Dim posicionUsuario = UsuariosTotales.IndexOf(usuario)
+        If posicionUsuario <> -1 Then
+            If usuario.Equals(UsuariosTotales(posicionUsuario)) Then
+                Return True
+            End If
+        End If
+        Return False
+    End Function
+End Class
